f5a2af37d4ece1edd8d6de9d90d6006ccb2c1630
unawarexi <unawarexi@gmail.com>
2024-07-29 11:44:29 +0100

    updated

---
+++ frontend/src/assets/Image.js
@@ -0,0 +1,5 @@
+// export { default as LoginBg } from "./food/pexels-ella-olsson-1640777.jpg";
+export { default as Logo } from "./food/logo.png";
+export { default as HeroBg } from "./food/cu6.png";
+
+// export { default as BgVideo } from "./food/landingVid.mp4";
